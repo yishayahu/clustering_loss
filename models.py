11e1517e0b81614b879324beaf416f5b3176b032
Yishayahu <ishayahu156@gmail.com>
2021-10-31 15:03:27 +0200

    f

--- models.py
+++ models.py
@@ -9,11 +9,15 @@ class DensNet(nn.Module):
         super().__init__()
         preloaded = torchvision.models.densenet121(pretrained=True)
         self.features = preloaded.features
-        self.classifier = nn.Linear(1024, num_classes, bias=True)
+        self.pre_classifier = nn.Sequential(nn.Linear(1024, 256, bias=True),
+                                            nn.BatchNorm1d(256),nn.ReLU())
+        self.classifier = nn.Linear(256, num_classes, bias=True)
+
         del preloaded
 
     def forward(self, x):
         features = self.features(x)
         out = F.relu(features, inplace=True)
         out = F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
+        out = self.pre_classifier(out)
         return self.classifier(out),out
